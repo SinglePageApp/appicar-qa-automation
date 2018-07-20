882df10e284145c81dbc6710d52fa90894487699
Matías J. Magni <matias.magni@gmail.com>
2018-07-20 20:32:54 -0300

    APQA-13: Test suites execution on same browser bug fixed.

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf results/reports/* && echo "All reports were deleted."
+rm -rf results/logs/* && echo "All logs were deleted."
+rm -rf results/screenshots/* && echo "All screenshots were deleted."
